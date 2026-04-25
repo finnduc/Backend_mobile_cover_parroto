5f44fcadeff5efb83a54d0db30c336d1c9286fdb
shynciee <hungd4842@gmail.com>
2026-04-25 14:23:01 +0700

    fix item card

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -34,6 +34,7 @@ android {
 }
 
 dependencies {
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation(libs.appcompat)
     implementation(libs.material)
