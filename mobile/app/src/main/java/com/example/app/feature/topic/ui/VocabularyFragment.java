ddc6a117631a88f2c309d6da94867781a597bba2
shynciee <hungd4842@gmail.com>
2026-04-25 15:45:18 +0700

    list lession card

--- mobile/app/src/main/java/com/example/app/feature/topic/ui/VocabularyFragment.java
+++ mobile/app/src/main/java/com/example/app/feature/topic/ui/VocabularyFragment.java
@@ -14,6 +14,6 @@ public class VocabularyFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_vocabulary, container, false);
+        return inflater.inflate(R.layout.fragment_lesson_list, container, false);
     }
 }
