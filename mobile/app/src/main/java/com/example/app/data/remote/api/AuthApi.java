3de54a836ce5f840b2c8fbc597c10983a9a3c55c
Hung Do <hungd4842@gmail.com>
2026-05-19 21:56:20 +0700

    fix apiremote

--- mobile/app/src/main/java/com/example/app/data/remote/api/AuthApi.java
+++ mobile/app/src/main/java/com/example/app/data/remote/api/AuthApi.java
@@ -1,11 +1,12 @@
 package com.example.app.data.remote.api;
 
 import com.example.app.data.remote.model.response.ApiResponse;
+import com.example.app.data.remote.model.response.user.UserResponse;
+
 import retrofit2.Call;
 import retrofit2.http.POST;
 
 public interface AuthApi {
-    // API đồng bộ user từ Firebase xuống Database Backend
-    @POST("auth/complete-signup")
-    Call<ApiResponse<String>> auth();
+    @POST("auth/sync")
+    Call<ApiResponse<UserResponse>> auth();
 }
