a5ee5187f31be7184eaf8f25a98822164b356444
gianghp123 <giangthpt123@gmail.com>
2026-06-07 16:25:17 +0700

    fix: use enums.UserRole type in AuthUserRes DTO

--- api/internal/modules/auth/dtos/res/auth.res.go
+++ api/internal/modules/auth/dtos/res/auth.res.go
@@ -1,10 +1,12 @@
 package res
 
-type UserRes struct {
-	ID        string `json:"id"`
-	Email     string `json:"email"`
-	Name      string `json:"name"`
-	UserRole  string `json:"user_role"`
-	AvatarURL string `json:"avatar_url"`
-	CreatedAt string `json:"created_at"`
+import "go-cover-parroto/internal/core/enums"
+
+type AuthUserRes struct {
+	ID        string         `json:"id"`
+	Email     string         `json:"email"`
+	Name      string         `json:"name"`
+	UserRole  enums.UserRole `json:"user_role"`
+	AvatarURL string         `json:"avatar_url"`
+	CreatedAt string         `json:"created_at"`
 }
